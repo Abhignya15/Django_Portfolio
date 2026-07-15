3e2ce7a8160d305a600ba22e983119678f106294
Abhignya15 <65651968+Abhignya15@users.noreply.github.com>
2026-07-15 23:06:49 +0530

    ecr

---
+++ Terraform/ecr.tf
@@ -0,0 +1,34 @@
+resource "aws_ecr_repository" "portfolio" {
+  name                 = "django-portfolio"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "portfolio" {
+  repository = aws_ecr_repository.portfolio.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description   = "Keep the 10 most recent images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+output "ecr_repository_url" {
+  description = "ECR repository URL used to tag and push Docker images"
+  value       = aws_ecr_repository.portfolio.repository_url
+}
