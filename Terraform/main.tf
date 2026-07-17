e8083167229db36698023542b62ba06ade1e3b9f
Abhignya15 <65651968+Abhignya15@users.noreply.github.com>
2026-07-17 20:08:24 +0530

    terraform update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,11 +1,19 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.10.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+  }
+
+  backend "s3" {
+    bucket       = "django-portfolio-terraform-state-376210054447"
+    key          = "portfolio/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
